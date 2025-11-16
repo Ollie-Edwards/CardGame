69741a760be506475e50f5adfbf4986c8477706c
Ollie-Edwards <ollie3edwards@gmail.com>
2025-11-16 17:56:05 +0000

    Lint existing code using 'Black' linter, and add python linting using 'Super Linter' and GiHub Actions workflow.

--- cards.py
+++ cards.py
@@ -1,61 +1,61 @@
 class Card:
-  def __init__(self, rank, suit):
-    self.rank = rank
-    self.suit = suit
-    self.value = self.get_value()
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = self.get_value()
 
-  def get_card_name(self):
-    return self.rank + " of " + self.suit
+    def get_card_name(self):
+        return self.rank + " of " + self.suit
 
-  def get_value(self):
-    ''' 
-      Face cards must be removed in triplets only
-      Ace has a value of 1
-      Number cards are worth their face value
-    '''
-    if self.rank in ["J", "Q", "K"]:
-      return 0
-    
-    elif self.rank == "A":
-      return 1
-    
-    elif self.rank.isdigit():
-      return int(self.rank)
-    
-    else:
-      print(self.rank)
-      raise Exception("Tried to find value of an unknown card")
+    def get_value(self):
+        """
+        Face cards must be removed in triplets only
+        Ace has a value of 1
+        Number cards are worth their face value
+        """
+        if self.rank in ["J", "Q", "K"]:
+            return 0
+
+        elif self.rank == "A":
+            return 1
+
+        elif self.rank.isdigit():
+            return int(self.rank)
+
+        else:
+            print(self.rank)
+            raise Exception("Tried to find value of an unknown card")
 
 
 class Deck:
-  def __init__(self):
-    self.cards = []
-    self.reset()
+    def __init__(self):
+        self.cards = []
+        self.reset()
 
-  def draw_card(self) -> int:
-    if self.cards == []:
-      return None
-    
-    return self.cards.pop()
-  
-  def reset(self) -> None:
-    self.cards = []
+    def draw_card(self) -> int:
+        if self.cards == []:
+            return None
 
-    suits = ["H", "D", "C", "S"]
-    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        return self.cards.pop()
 
-    for rank in ranks:
-      for suit in suits:
-        self.cards.append(Card(rank, suit))
+    def reset(self) -> None:
+        self.cards = []
 
-  def size(self) -> int:
-    return len(self.cards)
-  
-  def is_empty(self) -> bool:
-    return self.size() == 0
-  
-  def add_card(self, cards : list[Card]) -> None:
-    self.cards.extend(cards)
+        suits = ["H", "D", "C", "S"]
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-  def get_cards(self):
-    return self.cards
+        for rank in ranks:
+            for suit in suits:
+                self.cards.append(Card(rank, suit))
+
+    def size(self) -> int:
+        return len(self.cards)
+
+    def is_empty(self) -> bool:
+        return self.size() == 0
+
+    def add_card(self, cards: list[Card]) -> None:
+        self.cards.extend(cards)
+
+    def get_cards(self):
+        return self.cards
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from cards import Deck
 testDeck = Deck()
 
 for card in testDeck.get_cards():
-  print("name: ", card.get_card_name()," value: ", card.get_value())
+    print("name: ", card.get_card_name(), " value: ", card.get_value())
