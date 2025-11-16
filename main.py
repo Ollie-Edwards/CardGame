d32ad8de8a50103bf91206a6f4b002b110d91d4c
Ollie-Edwards <ollie3edwards@gmail.com>
2025-11-16 17:51:24 +0000

    Add card class, which will be used to represent a single card within a deck. Update the deck class to contain instances of the card class rather than integers

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-class Deck():
-  def __init__(self):
-    self.cards = [i for i in range(100)]
+from cards import Deck
 
-  def draw_card(self) -> int:
-    if self.cards == []:
-      return None
-    
-    return self.cards.pop()
-  
-  def reset(self) -> None:
-    self.cards = [i for i in range(100)]
+testDeck = Deck()
 
-  def size(self) -> int:
-    return len(self.cards)
-  
-  def is_empty(self) -> bool:
-    return self.size() == 0
-  
-  def add_card(self, cards : list[int]) -> None:
-    self.cards.extend(cards)
+for card in testDeck.get_cards():
+  print("name: ", card.get_card_name()," value: ", card.get_value())
