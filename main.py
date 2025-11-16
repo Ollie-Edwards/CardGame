8126b06dcec70dd0da9d9770c61cd7993c420d42
Ollie-Edwards <ollie3edwards@gmail.com>
2025-11-16 12:18:15 +0000

    add basic deck class, representing cards as integers for now

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from itertools import zip_longest
+import random
 
-# Card Game Initial Commit
+class Deck():
+  def __init__(self):
+    self.cards = [i for i in range(100)]
+
+  def draw_card(self) -> int:
+    if self.cards == []:
+      return None
+    
+    return self.cards.pop()
+  
+  def reset(self) -> None:
+    self.cards = [i for i in range(100)]
+
+  def size(self) -> int:
+    return len(self.cards)
+  
+  def is_empty(self) -> bool:
+    return self.size() == 0
+  
+  def add_card(self, cards : list[int]) -> None:
+    self.cards.extend(cards)
